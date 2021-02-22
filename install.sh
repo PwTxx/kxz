d2eed1b406171370d3f56313ec567f7758b20bf5
PwTxx <78833784+PwTxx@users.noreply.github.com>
2021-02-22 18:24:03 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,7 +9,13 @@ apt-get install wget
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm i imgbb-uploader
-npm cache clean -f
+echo "[INFO] If this error, try installing with --dev"
+echo ""
+echo "[Command] npm install --dev"
+echo "[INFO] That's a example commands with --dev."
+echo "[INFO] Then install got"
+echo "[Command] npm install got"
+echo ""
 npm install
 npm i got
 
